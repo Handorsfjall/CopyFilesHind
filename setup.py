be303f14c9f07d73db713a7a7b7b080f414a1943
Shubenkovll <79252013+Shubenkovll@users.noreply.github.com>
2021-06-15 02:17:59 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+import CopyFilesHind
+
+with open('README.md') as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name='CopyFilesHind',
+	version='1.0.0',
+	author='Hindarsfjall',
+	author_email='shubenkov@tuta.io',
+	description='With this code, you can copy all the files from your repository and find the largest of copied files.',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	packages=['CopyFilesHind'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+	platforms=["any"]
+)
